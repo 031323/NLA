f08fea7318acd30dd47025c318bb18974a2534cb
142434 <utkars.rajput@gmail.com>
2019-09-23 07:02:50 +0530

    6

--- element.py
+++ element.py
@@ -2,11 +2,18 @@ import numpy
 import math
 A=None
 B=None
-def homogenousneumann(mesh,f,h):
-    n_nodes=len(mesh.nodes)
+def homogenousdirichlet(mesh,f,fixed):
+    node_map=[]
+    n_free_nodes=0
+    for i in range(0,len(mesh.nodes)):
+        if fixed(i):
+            node_map.append(-1)
+        else:
+            node_map.append(n_free_nodes)
+            n_free_nodes+=1
     global A,B
-    A=numpy.zeros((n_nodes,n_nodes))
-    B=numpy.zeros(n_nodes);
+    A=numpy.zeros((n_free_nodes,n_free_nodes))
+    B=numpy.zeros(n_free_nodes);
     for triangle in mesh.triangles:
         gradients=[]
         solute=[]
@@ -18,17 +25,18 @@ def homogenousneumann(mesh,f,h):
             gradients.append(numpy.linalg.solve(numpy.matrix(solute),numpy.array(basis))[:2])
         for i in range(0,3):
             for j in range(i,3):
-                A[triangle[i],triangle[j]]+=numpy.dot(gradients[i],gradients[j]);
-                A[triangle[j],triangle[i]]+=A[triangle[i],triangle[j]]
+                if fixed(triangle[i]) or fixed(triangle[j]):
+                    continue
+                addition=numpy.dot(gradients[i],gradients[j]);
+                A[node_map[triangle[i]],node_map[triangle[j]]]+=addition
+                if i!=j:
+                    A[node_map[triangle[j]],node_map[triangle[i]]]+=addition
         area=abs(numpy.cross(numpy.subtract(solute[1][:2],solute[0][:2]),numpy.subtract(solute[2][:2],solute[0][:2])))/2
         centroid=[(solute[0][0]+solute[1][0]+solute[2][0])/3,(solute[0][1]+solute[1][1]+solute[2][1])/3]
         for vertex in triangle:
-            B[vertex]+=area*f(centroid[0],centroid[1])/3
-        for i in range(0,3):
-            for j in range(i+1,3):
-                if (h(triangle[i])!=0) and h(triangle[j]!=0):
-                    B[triangle[i]]+=h(triangle[i])*numpy.linalg.norm(numpy.subtract(mesh.nodes[triangle[i]],mesh.nodes[triangle[j]]))
-                    B[triangle[j]]+=h(triangle[j])*numpy.linalg.norm(numpy.subtract(mesh.nodes[triangle[i]],mesh.nodes[triangle[j]]))
+            if fixed(vertex):
+                continue
+            B[node_map[vertex]]+=area*f(centroid[0],centroid[1])/3
     return numpy.linalg.solve(A,B)
     
 class mesh:
@@ -48,40 +56,45 @@ for i in range(0,L1):
             v10=L2*i+j-1
             test_mesh.triangles+=[[v00,v01,v11],[v00,v11,v10]]
 def test_h(i):
-    if i<L2:
-        return (i-L2/2)*0.00001
+    if i<L2 or i>L2*(L1-1) or i%L2==0 or (i+1)%L2==0:
+        return 1
     else:
         return 0
 def test_f(x,y):
-    return 0
-    if x<y:
+    #return 1
+    #if x<y:
+    #    return 1
+    #else:
+    #    return 0
+    #return (x-L1/2)*1+(y-L2/2)*4
+    if x>L1/2:
         return 1
     else:
-        return -1
-    #return (x-L1/2)*1+(y-L2/2)*4
-    
-from numpy import exp,arange
-from pylab import meshgrid,cm,imshow,contour,clabel,colorbar,axis,title,show
+        return 0
 
-x=arange(5,L1,1)
-y=arange(1,L2-1,1)
-X,Y=meshgrid(x,y)
-#Z=test_f(X,Y)
-#im=imshow(Z)
+solve=homogenousdirichlet(test_mesh,test_f,test_h)
 
-solve=homogenousneumann(test_mesh,test_f,test_h)
-def solve_f(x,y):
-    return (solve[x*L2+y])
-Z2=solve_f(X,Y)
-minz=numpy.min(Z2)
-if minz<0:
-    minz=abs(minz)+0.001
-else:
-    minz=0.001
-print(Z2)
-for i in range(0,len(Z2)):
-    for j in range(0,len(Z2[0])):
-        #if Z2[i][j]>0:
-        Z2[i][j]=Z2[i][j]#math.log(minz+(Z2[i][j]))
-im2=imshow(Z2)
-show()
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits import mplot3d
+
+bafar=0
+b = numpy.arange(bafar, L2-bafar, 1)
+d = numpy.arange(bafar, L1-bafar, 1)
+nu = numpy.zeros( (b.size, d.size) )
+counter= 0
+
+for i in range(bafar,L1-bafar):
+    for j in range(bafar,L2-bafar):
+        if not test_h(i*L2+j):
+            nu[i-bafar][j-bafar]=solve[counter]
+            counter+=1
+
+X, Y = numpy.meshgrid(d, b)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_proj_type('persp')
+ax.plot_surface(X, Y, nu,cmap=cm.RdBu)
+plt.show()
+
