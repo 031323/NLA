0d93c2878c6185434bb6af690d002028c35f5bcf
142434 <utkars.rajput@gmail.com>
2021-01-19 10:36:14 +0530

    errorindicator

--- element/element.py
+++ element/element.py
@@ -1,5 +1,6 @@
 import numpy
 import math
+from matplotlib import cm
 A=None
 B=None
 def pxy(degree,c,x,y):
@@ -77,13 +78,13 @@ gq2d=[
 
 ]
 
-
-def solve(mesh,degree,boundary,f,fixed,g=lambda x:0,h=lambda x:0):
+error=float('inf')
+def solve(mesh,degree,boundary,f,fixed,element_type,g=lambda x:0,h=lambda x:0,P1_skip=[]):
 	node_map=[]
 	#global n_free_nodes
 	n_free_nodes=0
 	for i in range(0,len(mesh.nodes)):
-		if fixed(i):
+		if fixed(i) or (element_type=='P1' and P1_skip[i]):
 			node_map.append(-1)
 		else:
 			node_map.append(n_free_nodes)
@@ -91,10 +92,12 @@ def solve(mesh,degree,boundary,f,fixed,g=lambda x:0,h=lambda x:0):
 	global A,B
 	A=numpy.zeros((n_free_nodes,n_free_nodes))
 	B=numpy.zeros(n_free_nodes);
+	Ms=[]
 	for tn,triangle in enumerate(mesh.triangles):
 		v=[mesh.nodes[mesh.triangle_vertices[tn][0]],mesh.nodes[mesh.triangle_vertices[tn][1]],mesh.nodes[mesh.triangle_vertices[tn][2]]]
 		area=abs(numpy.cross(numpy.subtract(v[1],v[0]),numpy.subtract(v[2],v[0])))*0.5
 		M=[]
+		#print(triangle)
 		for node in triangle:
 			[x,y]=mesh.nodes[node]
 			Mi=[]
@@ -119,8 +122,10 @@ def solve(mesh,degree,boundary,f,fixed,g=lambda x:0,h=lambda x:0):
 						Dy=(node2[1]-y)/l
 						Mi.append((j-k)*pow(x,max(j-k-1,0))*pow(y,k)*Dx+Dy*(k)*pow(x,j-k)*pow(y,max(k-1,0)))
 					'''
+			#print(M)
 			M.append(Mi)
 		ce=numpy.linalg.inv(M)
+		Ms.append(ce)
 		
 		#  a(ei,ej) in omega
 		
@@ -200,10 +205,88 @@ def solve(mesh,degree,boundary,f,fixed,g=lambda x:0,h=lambda x:0):
 	#print("A:\n",A,"\nB:\n",B)
 	if n_free_nodes==len(mesh.nodes):
 		print("lstsq")
-		return numpy.linalg.lstsq(A,B)[0]
+		node_values=numpy.linalg.lstsq(A,B)[0]
 	else:
 		print("solve")
-		return numpy.linalg.solve(A,B)
+		node_values=numpy.linalg.solve(A,B)
+	
+	#Eriksson-Johnson error indicator
+	def coeff(i):
+		node_z=[0,0,0]
+		for j in range(0,3):
+			if fixed(mesh.triangles[i][j]):
+				node_z[j]=g(mesh.triangles[i][j])
+			else:
+				node_z[j]=node_values[node_map[mesh.triangles[i][j]]]
+		return numpy.matmul(Ms[i],numpy.transpose([node_z]))
+	if degree==1 and element_type=='lagrange':
+		maxerr=0
+		for n in range(0,len(mesh.nodes)):
+			if fixed(n):continue
+			maxerr=max(maxerr,abs(2*mesh.nodes[n][0]*(1-mesh.nodes[n][0])-node_values[node_map[n]]))
+		print('Linf '+str(maxerr))
+		triangle_error=numpy.full(len(mesh.triangles),numpy.inf)
+		du_x=numpy.full(len(mesh.triangles),0.0)
+		du_y=numpy.full(len(mesh.triangles),0.0)
+		c=numpy.full((len(mesh.triangles),2),0.0)
+		for tn,t in enumerate(mesh.triangles):
+			for j in range(0,3):
+				c[tn]+=numpy.array(mesh.nodes[mesh.triangle_vertices[tn][j]])
+			c[tn]/=3
+			coeffs=coeff(tn)
+			du_x[tn]=dx_pxy(degree,coeffs,c[tn][0],c[tn][1])
+			du_y[tn]=dy_pxy(degree,coeffs,c[tn][0],c[tn][1])
+		maxerr=0
+		maxtn=[0,0]
+		for tn1 in range(0,len(mesh.triangles)):
+			for tn2 in range(tn1+1,len(mesh.triangles)):
+				n_common=0
+				for i in range(0,3):
+					for j in range(0,3):
+						if mesh.triangles[tn1][i]==mesh.triangles[tn2][j]:n_common+=1
+				if n_common!=2:continue
+				distance=numpy.linalg.norm(c[tn1]-c[tn2])
+				if max(abs(du_x[tn1]-du_x[tn2])/distance,abs(du_y[tn1]-du_y[tn2])/distance)>maxerr:maxtn=[tn1,tn2]
+				maxerr=max(maxerr,abs(du_x[tn1]-du_x[tn2])/distance,abs(du_y[tn1]-du_y[tn2])/distance)
+		print('error: '+str(maxerr))
+		print('maxtnc')
+		print(c[maxtn[0]],c[maxtn[1]])
+					
+				
+	
+	if element_type=='P1':
+		tri_points=[]
+		for i in range(0,len(Ms)):
+			node_z=[0,0,0]
+			for j in range(0,3):
+				if fixed(mesh.triangles[i][j]):
+					node_z[j]=g(mesh.triangles[i][j])
+				else:
+					node_z[j]=node_values[node_map[mesh.triangles[i][j]]]
+			coeffs=numpy.matmul(Ms[i],numpy.transpose([node_z]))
+			print(coeffs)
+			print ("")
+			tn=i
+			v=[mesh.nodes[mesh.triangle_vertices[tn][0]],mesh.nodes[mesh.triangle_vertices[tn][1]],mesh.nodes[mesh.triangle_vertices[tn][2]]]
+			for j in range(0,3):
+				v_z=coeffs[0]+coeffs[1]*v[j][0]+coeffs[2]*v[j][1]
+				tri_points+=[(v[j][0],v[j][1],v_z[0])]
+		
+		from itertools import chain
+		import matplotlib.pyplot as plt
+		from mpl_toolkits.mplot3d import Axes3D
+# Get the X, Y and Z coordinates of each point
+		print(tri_points[0])
+		x, y, z = zip(*tri_points)
+# Make list of triangle indices ([(0, 1, 2), (3, 4, 5), ...])
+		tri_idx = [(3 * i, 3 * i + 1, 3 * i + 2) for i in range(int(len(tri_points)/3))]
+# Make 3D axes
+		ax = plt.figure().gca(projection='3d')
+# Plot triangles
+		ax.plot_trisurf(x, y, z, triangles=tri_idx,cmap=cm.RdBu)
+		plt.show()
+
+	return node_values	
 	#A=numpy.delete(A,0,0)
 	#A=numpy.delete(A,0,1)
 	#B=numpy.delete(B,0,0)
@@ -218,21 +301,23 @@ class mesh:
 	triangle_vertices=[]
 
 test_mesh=mesh()
-element_type='hermite'
+element_type='lagrange'
 # hermite triangle pair
 # 0 1 1 0
 # 1 0 1 1
 # 1 1 0 1
 # 0 1 1 0
-degree=3
+degree=1
 if element_type=='hermite':degree=3
+if element_type=='P1':degree=1
 l1=1
 l2=1
-n_block_per_side=4
-if element_type=='lagrange':
-	L1=n_block_per_side*degree*2+1
-	L2=n_block_per_side*degree*2+1
-	spe=degree # sections per edge
+n_block_per_side=10
+if element_type=='lagrange' or element_type=='P1':
+	if element_type=='lagrange':spe=degree # sections per edge
+	else: spe=2
+	L1=n_block_per_side*spe*2+1
+	L2=n_block_per_side*spe*2+1
 else:
 	L1=n_block_per_side*1*2+1
 	L2=n_block_per_side*1*2+1
@@ -241,10 +326,14 @@ n_elements2=0
 hermite_map=[]
 hermite_extra_nodes=[]
 hermite_extra_node_type=[]
+P1_skip=[]
 for i in range(0,L1):
 	for j in range(0,L2):
 		test_mesh.nodes.append([i/(L1-1)*l1,j/(L2-1)*l2])
 		test_mesh.node_type.append(' ')
+		if element_type=='P1':
+			if i%spe==0 and j%spe==0:P1_skip+=[True]
+			else: P1_skip+=[False]
 		if element_type=='hermite':
 			hermite_map+=[len(hermite_extra_nodes)]
 			hermite_extra_nodes.append([i/(L1-1)*l1,j/(L2-1)*l2])
@@ -259,6 +348,7 @@ for i in range(0,L1):
 			t2=[]
 			for i_ in range(i-spe,i+1):
 				for j_ in range(j-spe,j+1):
+					if element_type=='P1' and (i_==i-spe or i_==i ) and (j_==j-spe or j_==j):continue
 					node=L2*i_+j_
 					if F2:
 						if (i-i_+j-j_>=spe):
@@ -310,7 +400,7 @@ def test_boundary(i):
 	return test_mesh.nodes[i][0]==0 or test_mesh.nodes[i][0]==1 or test_mesh.nodes[i][1]==0 or test_mesh.nodes[i][1]==1
 	#return i<L2 or i>=L2*(L1-1) or i%L2==0 or (i+1)%L2==0
 def test_fixed(i):
-	return test_mesh.nodes[i][0]==0
+	#return test_mesh.nodes[i][0]==0 or test_mesh.nodes[i][0]==1
 	if i<L2:return 1
 	else:return 0
 	#if i==L2/2 or i==L2/2-1 :return 1
@@ -328,6 +418,7 @@ def test_fixed(i):
 	else:
 		return 0
 def test_g(i):
+	#return 0
 	return i/(L2-1)
 	if i<L2:
 		return i/L2
@@ -342,7 +433,7 @@ def test_g(i):
 		return 0
 def test_f(x,y):
 	return 4
-	#return -2*x*x-2*y*y
+	return -2*x*x-2*y*y
 	if x>y:
 		return 40
 	else:
@@ -355,6 +446,7 @@ def test_f(x,y):
 def test_xy(i):
 	return test_mesh.nodes[i][0],test_mesh.nodes[i][1]
 def test_h(i,towards):
+	#return 0
 	return -1
 	x,y=test_xy(i)
 	#if (x==0 or x==1) and (y==0 or y==1):
@@ -393,7 +485,8 @@ def test_h(i,towards):
 		return -1
 	return i*(L2-1-i)/L2/L2*10
 
-solve=solve(test_mesh,degree,test_boundary,test_f,test_fixed,test_g,test_h)
+solve=solve(test_mesh,degree,test_boundary,test_f,test_fixed,element_type,test_g,test_h,P1_skip)
+
 
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -409,11 +502,13 @@ counter= 0
 
 for i in range(bafar,L1-bafar):
 	for j in range(bafar,L2-bafar):
-		if not test_fixed(i*L2+j):
+		if not (test_fixed(i*L2+j) or (element_type=='P1' and P1_skip[i*L2+j])):
 			nu[i-bafar][j-bafar]=solve[counter]
 			counter+=1
-		else:
+		elif element_type != 'P1':
 			nu[i-bafar][j-bafar]=test_g(i*L2+j)
+		else:
+			nu[i-bafar][j-bafar]=0
 		
 off=nu[0][0]
 for i in range(0,L1-0):
@@ -423,14 +518,17 @@ for i in range(0,L1-0):
 
 X, Y = numpy.meshgrid(b, d)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.set_proj_type('persp')
-ax.plot_surface(Y, X, nu,cmap=cm.RdBu)
-plt.show()
+if element_type!='P1':fig = plt.figure()
+if element_type!='P1':ax = fig.add_subplot(111, projection='3d')
+if element_type!='P1':ax.set_proj_type('persp')
+if element_type!='P1':ax.plot_surface(Y, X, nu,cmap=cm.RdBu)
+if element_type!='P1':plt.show()
+	
 
 def XC(x1,x2):
 	for i in range(len(x1)):
 		for j in range(len(x1[0])):
 			if abs(x1[i][j]-x2[i*2][j*2])<0.0001:
 				print(i,j)
+
+
